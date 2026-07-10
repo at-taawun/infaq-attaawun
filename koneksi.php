d6201a887652c938871920316e12e4806a899606
at-taawun <masjidattaawun7@gmail.com>
2026-07-10 10:56:54 +0700

    Create koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+  //con OPR
+$opr=mysqli_connect('192.168.27.222','root','Int3l','infaq');
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+  ?>
